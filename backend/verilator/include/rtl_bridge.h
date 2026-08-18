f728e4805c944b842fa988eac8edb2d0a8160709
dirinkbottle <2909128143@qq.com>
2026-08-18 18:53:26 +0800

    wire AXI RAM write channel through verilator bridge

--- backend/verilator/include/rtl_bridge.h
+++ backend/verilator/include/rtl_bridge.h
@@ -21,11 +21,9 @@ bool rtl_bridge_reg_read(uint32_t index, uint32_t *value);
 bool rtl_bridge_csr_read(uint32_t index, uint32_t *value);
 bool rtl_bridge_csr_write(uint32_t index, uint32_t value);
 
-bool rtl_bridge_set_axi_rom_arready(bool);
-bool rtl_bridge_set_axi_rom_rvalid(bool);
+bool rtl_bridge_set_axi_rom_arready(bool value);
+bool rtl_bridge_set_axi_rom_rvalid(bool value);
 bool rtl_bridge_set_axi_rom_rdata(uint32_t data);
-
-// ROM AXI 接口 get 方法
 bool rtl_bridge_get_axi_rom_arready(void);
 bool rtl_bridge_get_axi_rom_rvalid(void);
 uint32_t rtl_bridge_get_axi_rom_rdata(void);
@@ -33,10 +31,13 @@ bool rtl_bridge_get_axi_rom_cpu_arvalid(void);
 bool rtl_bridge_get_axi_rom_cpu_rready(void);
 uint32_t rtl_bridge_get_axi_rom_cpu_araddr(void);
 
-// RAM AXI 接口 set/get 方法
-bool rtl_bridge_set_axi_ram_arready(bool);
-bool rtl_bridge_set_axi_ram_rvalid(bool);
+bool rtl_bridge_set_axi_ram_arready(bool value);
+bool rtl_bridge_set_axi_ram_rvalid(bool value);
 bool rtl_bridge_set_axi_ram_rdata(uint32_t data);
+bool rtl_bridge_set_axi_ram_awready(bool value);
+bool rtl_bridge_set_axi_ram_wready(bool value);
+bool rtl_bridge_set_axi_ram_bvalid(bool value);
+
 bool rtl_bridge_get_axi_ram_arready(void);
 bool rtl_bridge_get_axi_ram_rvalid(void);
 uint32_t rtl_bridge_get_axi_ram_rdata(void);
@@ -44,7 +45,13 @@ bool rtl_bridge_get_axi_ram_cpu_arvalid(void);
 bool rtl_bridge_get_axi_ram_cpu_rready(void);
 uint32_t rtl_bridge_get_axi_ram_cpu_araddr(void);
 
-// 访存对齐检测
+bool rtl_bridge_get_axi_ram_cpu_awvalid(void);
+uint32_t rtl_bridge_get_axi_ram_cpu_awaddr(void);
+bool rtl_bridge_get_axi_ram_cpu_wvalid(void);
+uint32_t rtl_bridge_get_axi_ram_cpu_wdata(void);
+uint8_t rtl_bridge_get_axi_ram_cpu_wstrb(void);
+bool rtl_bridge_get_axi_ram_cpu_bready(void);
+
 bool rtl_bridge_get_mem_addr_read_unalign(void);
 bool rtl_bridge_get_mem_addr_write_unalign(void);
 
