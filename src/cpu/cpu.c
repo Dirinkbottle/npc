e16ddf4077676925859d21c7b13e3c9de36c93f8
dirinkbottle <2909128143@qq.com>
2026-08-18 18:54:19 +0800

    add CPU-side wrappers for AXI RAM writes

--- src/cpu/cpu.c
+++ src/cpu/cpu.c
@@ -15,125 +15,59 @@ static SimState sim_state = SIM_STOP;
 static int exit_good = 0;
 uint64_t cpu_total_cycle = 0;
 
-void set_npc_state(SimState state) {
-  sim_state = state;
-}
-
-SimState get_npc_state(void) {
-  return sim_state;
-}
-
+void set_npc_state(SimState state) { sim_state = state; }
+SimState get_npc_state(void) { return sim_state; }
 void sim_abort(void) {
   if (sim_state != SIM_END) {
     sim_state = SIM_ABORT;
     exit_good = -1;
   }
 }
+bool sim_is_running(void) { return sim_state == SIM_RUNNING; }
+int sim_exit_code(void) { return exit_good; }
 
-bool sim_is_running(void) {
-  return sim_state == SIM_RUNNING;
-}
-
-int sim_exit_code(void) {
-  return exit_good;
-}
-
-bool init_simulator(int argc, char **argv) {
-  return rtl_bridge_init(argc, argv);
-}
-
-void destroy_simulator(void) {
-  rtl_bridge_destroy();
-}
+bool init_simulator(int argc, char **argv) { return rtl_bridge_init(argc, argv); }
+void destroy_simulator(void) { rtl_bridge_destroy(); }
 
 uint32_t cpu_current_pc(void) {
   const uint32_t pc = rtl_bridge_pc();
   return pc == 0u ? PMEM_BASE : pc;
 }
 
-bool cpu_reg_read(uint32_t index, uint32_t *value) {
-  return rtl_bridge_reg_read(index, value);
-}
+bool cpu_reg_read(uint32_t index, uint32_t *value) { return rtl_bridge_reg_read(index, value); }
+bool cpu_csr_read(uint32_t index, uint32_t *value) { return rtl_bridge_csr_read(index, value); }
+bool cpu_csr_write(uint32_t index, uint32_t value) { return rtl_bridge_csr_write(index, value); }
 
-bool cpu_csr_read(uint32_t index, uint32_t *value) {
-  return rtl_bridge_csr_read(index, value);
-}
+bool cpu_axi_set_rom_arready(bool value) { return rtl_bridge_set_axi_rom_arready(value); }
+bool cpu_axi_set_rom_rvalid(bool value) { return rtl_bridge_set_axi_rom_rvalid(value); }
+bool cpu_axi_set_rom_rdata(uint32_t data) { return rtl_bridge_set_axi_rom_rdata(data); }
+bool cpu_axi_get_rom_arready(void) { return rtl_bridge_get_axi_rom_arready(); }
+bool cpu_axi_get_rom_rvalid(void) { return rtl_bridge_get_axi_rom_rvalid(); }
+uint32_t cpu_axi_get_rom_rdata(void) { return rtl_bridge_get_axi_rom_rdata(); }
+bool cpu_axi_get_rom_cpu_arvalid(void) { return rtl_bridge_get_axi_rom_cpu_arvalid(); }
+bool cpu_axi_get_rom_cpu_rready(void) { return rtl_bridge_get_axi_rom_cpu_rready(); }
+uint32_t cpu_axi_get_rom_cpu_araddr(void) { return rtl_bridge_get_axi_rom_cpu_araddr(); }
 
-bool cpu_csr_write(uint32_t index, uint32_t value) {
-  return rtl_bridge_csr_write(index, value);
-}
+bool cpu_axi_set_ram_arready(bool value) { return rtl_bridge_set_axi_ram_arready(value); }
+bool cpu_axi_set_ram_rvalid(bool value) { return rtl_bridge_set_axi_ram_rvalid(value); }
+bool cpu_axi_set_ram_rdata(uint32_t data) { return rtl_bridge_set_axi_ram_rdata(data); }
+bool cpu_axi_set_ram_awready(bool value) { return rtl_bridge_set_axi_ram_awready(value); }
+bool cpu_axi_set_ram_wready(bool value) { return rtl_bridge_set_axi_ram_wready(value); }
+bool cpu_axi_set_ram_bvalid(bool value) { return rtl_bridge_set_axi_ram_bvalid(value); }
 
-bool cpu_axi_set_rom_arready(bool value) {
-  return rtl_bridge_set_axi_rom_arready(value);
-}
+bool cpu_axi_get_ram_arready(void) { return rtl_bridge_get_axi_ram_arready(); }
+bool cpu_axi_get_ram_rvalid(void) { return rtl_bridge_get_axi_ram_rvalid(); }
+uint32_t cpu_axi_get_ram_rdata(void) { return rtl_bridge_get_axi_ram_rdata(); }
+bool cpu_axi_get_ram_cpu_arvalid(void) { return rtl_bridge_get_axi_ram_cpu_arvalid(); }
+bool cpu_axi_get_ram_cpu_rready(void) { return rtl_bridge_get_axi_ram_cpu_rready(); }
+uint32_t cpu_axi_get_ram_cpu_araddr(void) { return rtl_bridge_get_axi_ram_cpu_araddr(); }
 
-bool cpu_axi_set_rom_rvalid(bool value) {
-  return rtl_bridge_set_axi_rom_rvalid(value);
-}
-
-bool cpu_axi_set_rom_rdata(uint32_t data) {
-  return rtl_bridge_set_axi_rom_rdata(data);
-}
-
-bool cpu_axi_get_rom_arready(void) {
-  return rtl_bridge_get_axi_rom_arready();
-}
-
-bool cpu_axi_get_rom_rvalid(void) {
-  return rtl_bridge_get_axi_rom_rvalid();
-}
-
-uint32_t cpu_axi_get_rom_rdata(void) {
-  return rtl_bridge_get_axi_rom_rdata();
-}
-
-bool cpu_axi_get_rom_cpu_arvalid(void) {
-  return rtl_bridge_get_axi_rom_cpu_arvalid();
-}
-
-bool cpu_axi_get_rom_cpu_rready(void) {
-  return rtl_bridge_get_axi_rom_cpu_rready();
-}
-
-uint32_t cpu_axi_get_rom_cpu_araddr(void) {
-  return rtl_bridge_get_axi_rom_cpu_araddr();
-}
-
-bool cpu_axi_set_ram_arready(bool value) {
-  return rtl_bridge_set_axi_ram_arready(value);
-}
-
-bool cpu_axi_set_ram_rvalid(bool value) {
-  return rtl_bridge_set_axi_ram_rvalid(value);
-}
-
-bool cpu_axi_set_ram_rdata(uint32_t data) {
-  return rtl_bridge_set_axi_ram_rdata(data);
-}
-
-bool cpu_axi_get_ram_arready(void) {
-  return rtl_bridge_get_axi_ram_arready();
-}
-
-bool cpu_axi_get_ram_rvalid(void) {
-  return rtl_bridge_get_axi_ram_rvalid();
-}
-
-uint32_t cpu_axi_get_ram_rdata(void) {
-  return rtl_bridge_get_axi_ram_rdata();
-}
-
-bool cpu_axi_get_ram_cpu_arvalid(void) {
-  return rtl_bridge_get_axi_ram_cpu_arvalid();
-}
-
-bool cpu_axi_get_ram_cpu_rready(void) {
-  return rtl_bridge_get_axi_ram_cpu_rready();
-}
-
-uint32_t cpu_axi_get_ram_cpu_araddr(void) {
-  return rtl_bridge_get_axi_ram_cpu_araddr();
-}
+bool cpu_axi_get_ram_cpu_awvalid(void) { return rtl_bridge_get_axi_ram_cpu_awvalid(); }
+uint32_t cpu_axi_get_ram_cpu_awaddr(void) { return rtl_bridge_get_axi_ram_cpu_awaddr(); }
+bool cpu_axi_get_ram_cpu_wvalid(void) { return rtl_bridge_get_axi_ram_cpu_wvalid(); }
+uint32_t cpu_axi_get_ram_cpu_wdata(void) { return rtl_bridge_get_axi_ram_cpu_wdata(); }
+uint8_t cpu_axi_get_ram_cpu_wstrb(void) { return rtl_bridge_get_axi_ram_cpu_wstrb(); }
+bool cpu_axi_get_ram_cpu_bready(void) { return rtl_bridge_get_axi_ram_cpu_bready(); }
 
 void ebreak(void) {
   uint32_t a0 = 0;
@@ -147,7 +81,6 @@ void ebreak(void) {
 }
 
 static void exec_once(bool view_trace) {
-  /* Loads and instruction fetches settle while the clock is low. */
   rtl_bridge_set_clock(false);
   rtl_bridge_eval();
 
@@ -158,14 +91,6 @@ static void exec_once(bool view_trace) {
     itrace_print(pc, inst);
   }
 
-  // TODO: 目前测试axi总线暂时关闭
-  // if (rtl_bridge_invalid()) {
-  //   printf(FMT_RED "Invalid instruction at pc 0x%08x" FMT_NONE "\n", pc);
-  //   sim_abort();
-  //   return;
-  // }
-
-  /* Rising edge commits the architectural instruction. */
   rtl_bridge_set_clock(true);
   rtl_bridge_eval();
 
@@ -186,8 +111,7 @@ static void exec_once(bool view_trace) {
 static void cpu_update_total_cycle(void) {
   uint32_t mcycle = 0;
   uint32_t mcycleh = 0;
-  if (!cpu_csr_read(0xb00u, &mcycle) ||
-      !cpu_csr_read(0xb80u, &mcycleh)) {
+  if (!cpu_csr_read(0xb00u, &mcycle) || !cpu_csr_read(0xb80u, &mcycleh)) {
     return;
   }
   cpu_total_cycle = ((uint64_t)mcycleh << 32) | mcycle;
