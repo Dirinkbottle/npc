b19416fb28dba8015dc9c7b76acfc42de274d47f
dirinkbottle <2909128143@qq.com>
2026-08-18 18:53:37 +0800

    expose AXI RAM write channel to C memory model

--- include/cpu.h
+++ include/cpu.h
@@ -28,7 +28,7 @@ bool cpu_csr_read(uint32_t index, uint32_t *value);
 bool cpu_csr_write(uint32_t index, uint32_t value);
 void cpu_exec(uint64_t n);
 
-/* AXI 总线接口：ROM/RAM 各一组，set 写 RTL 输入，get 读 RTL 信号。 */
+/* AXI 总线接口：set 写 RTL 输入，get 读 RTL 输出。 */
 bool cpu_axi_set_rom_arready(bool value);
 bool cpu_axi_set_rom_rvalid(bool value);
 bool cpu_axi_set_rom_rdata(uint32_t data);
@@ -42,16 +42,27 @@ uint32_t cpu_axi_get_rom_cpu_araddr(void);
 bool cpu_axi_set_ram_arready(bool value);
 bool cpu_axi_set_ram_rvalid(bool value);
 bool cpu_axi_set_ram_rdata(uint32_t data);
+bool cpu_axi_set_ram_awready(bool value);
+bool cpu_axi_set_ram_wready(bool value);
+bool cpu_axi_set_ram_bvalid(bool value);
+
 bool cpu_axi_get_ram_arready(void);
 bool cpu_axi_get_ram_rvalid(void);
 uint32_t cpu_axi_get_ram_rdata(void);
 bool cpu_axi_get_ram_cpu_arvalid(void);
 bool cpu_axi_get_ram_cpu_rready(void);
 uint32_t cpu_axi_get_ram_cpu_araddr(void);
+
+bool cpu_axi_get_ram_cpu_awvalid(void);
+uint32_t cpu_axi_get_ram_cpu_awaddr(void);
+bool cpu_axi_get_ram_cpu_wvalid(void);
+uint32_t cpu_axi_get_ram_cpu_wdata(void);
+uint8_t cpu_axi_get_ram_cpu_wstrb(void);
+bool cpu_axi_get_ram_cpu_bready(void);
+
 extern uint8_t cpu_clk;
 extern uint8_t cpu_rst;
 extern uint64_t cpu_total_cycle;
-/* DPI-C entry point called by the RTL on EBREAK. */
 void ebreak(void);
 
 #endif
